5c5b409adb2a25df53577f1c6ead4a573fb5c289
MullerLucas <muller.lucas@mailbox.org>
2020-05-26 17:38:26 +0200

    - adjust colors and linehight

--- source/config.def.h
+++ source/config.def.h
@@ -13,25 +13,26 @@ static const char *fonts[] = {
 #define COLOR_BLACK "#111111"
 #define COLOR_RED "#ff4949"
 #define COLOR_GREEN "#c3ff67"
-#define COLOR_YELLOW "#af865a"
-#define COLOR_BLUE "#65a7be"
-#define COLOR_MAGENTA "#085f9a"
-#define COLOR_CYAN "#365d5d"
+#define COLOR_YELLOW "#eddd58"
+#define COLOR_BLUE "#4da6c5"
+#define COLOR_MAGENTA "#e85f9a"
+#define COLOR_CYAN "#558867"
 #define COLOR_WHITE "#eeeeee"
+// #define COLOR_WHITE "#fff8e5"
 
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 				/*     fg         bg       */
-	[SchemeNorm] = 		{ COLOR_BLACK, COLOR_WHITE },
-	[SchemeSel] = 		{ COLOR_WHITE, COLOR_BLACK},
-	[SchemeSelHighlight] = 	{ COLOR_GREEN, COLOR_BLACK},
-	[SchemeNormHighlight] = { COLOR_RED, COLOR_WHITE},
+	[SchemeNorm] = 		{ COLOR_WHITE, COLOR_BLACK},
+	[SchemeSel] = 		{ COLOR_BLACK, COLOR_GREEN},
+	[SchemeSelHighlight] = 	{ COLOR_RED,   COLOR_GREEN},
+	[SchemeNormHighlight] = { COLOR_RED,   COLOR_BLACK},
 	// TODO: Find out what this is
 	[SchemeOut] = 		{ "#000000", "#00ffff"     },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
 static unsigned int lines      = 0;
-static unsigned int lineheight = 25;         /* -h option; minimum height of a menu line     */
+static unsigned int lineheight = 27;         /* -h option; minimum height of a menu line     */
 
 /*
  * Characters not considered part of a word while deleting words
